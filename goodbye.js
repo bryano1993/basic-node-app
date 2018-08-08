2c56a694b4f568ad34f88227850a8b392e00cd20
Bryan O'Connor <bryano1993@gmail.com>
2018-08-08 15:40:54 -0700

    basic node app:complete

---
+++ goodbye.js
@@ -0,0 +1,9 @@
+exports.goodByeEnglish = function() {
+  console.log("Good bye");
+};
+
+exports.goodByeJapanese = function() {
+  console.log("Sayōnara");
+};
+
+exports = "bye";
---
+++ main.js
@@ -0,0 +1,15 @@
+const greetings = require("./greeting.js");
+
+const goodbye = require("./goodbye.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+goodbye.goodByeEnglish();
+
+goodbye.goodByeJapanese();
